7ad8be48f4bd923951ae44d13e642e50520d361d
gp_ <getor3@gmail.com>
2025-02-10 19:23:58 -0500

    added database

--- app.py
+++ app.py
@@ -1,14 +1,49 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect, url_for
 from docx import Document
 import os
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-# Path to the template file (Ensure 'template.docx' exists in the same directory)
+db = SQLAlchemy(app)
+
 TEMPLATE_PATH = "template.docx"
 
-# Added on 02 09 2025
+class FormData(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    PTO = db.Column(db.String(100), nullable=False)
+    HOURS = db.Column(db.String(100), nullable=False)
+    
+
+    def __init__(self, PTO, HOURS):
+        self.PTO = PTO
+        self.HOURS = HOURS
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/records', methods=['GET', 'POST'])
+def index():
+    
+    if request.method == 'POST':
+        
+        name_pto = request.form['pto']
+        hours_pto = request.form['hours']
+        Type_hours = FormData(name_pto, hours_pto)
+
+        db.session.add(Type_hours)
+        db.session.commit()
+
+        return redirect(url_for('success'))
+
+    return render_template('past_requests.html')
+
+@app.route('/success')
+def success():
+    return 'Form data submitted successfully!'
 
 @app.route('/')
 def home():
@@ -16,26 +51,25 @@ def home():
     formatted_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
     return render_template("home.html", current_date_time=formatted_date_time)
 
-@app.route('/form')
-def form():
-    return render_template("form.html")
 
 @app.route('/past_request')
 def past_request():
     return render_template("past_request.html")
 
+@app.route('/form')
+def form():
+    return render_template("form.html")
 
 @app.route('/generate-doc', methods=['POST'])
 def generate_doc():
+   
     try:
-        # Get user input
+
         emp_name = request.form.get('emp_name', 'John Doe')
         todays_date = request.form.get('todays_date', 'YYYY-MM-DD')
         super_name_one = request.form.get('super_name_one', 'Supervisors name')
         super_name_two = request.form.get('super_name_two', 'Supervisors name')
         badge = request.form.get('badge', 'badge')
-        
-# Added on 02 06 2025 
         start_date = request.form.get('start_date', 'start_date')
         return_date = request.form.get('return_date', 'return_date')
         vac_hours = request.form.get('vac_hours', 'vac_hours')
@@ -49,19 +83,14 @@ def generate_doc():
         sick_start_date = request.form.get('sick_start_date', 'sick_start_date')
         sick_hours = request.form.get('sick_hours', 'sick_hours')
         
-        
-        # Load the template
         doc = Document(TEMPLATE_PATH)
 
-        # Replace placeholders in the template
         for paragraph in doc.paragraphs:
             paragraph.text = paragraph.text.replace("{emp_name}", emp_name)
             paragraph.text = paragraph.text.replace("{todays_date}", todays_date)
             paragraph.text = paragraph.text.replace("{super_name_one}", super_name_one)
             paragraph.text = paragraph.text.replace("{super_name_two}", super_name_two)
             paragraph.text = paragraph.text.replace("{badge}", badge)
-
-# Added on 02 06 2025
             paragraph.text = paragraph.text.replace("{start_date}", start_date)
             paragraph.text = paragraph.text.replace("{return_date}", return_date)
             paragraph.text = paragraph.text.replace("{vac_hours}", vac_hours)
@@ -75,15 +104,14 @@ def generate_doc():
             paragraph.text = paragraph.text.replace("{sick_start_date}", sick_start_date)
             paragraph.text = paragraph.text.replace("{sick_hours}", sick_hours)             
         
-        # Save the modified document
+      
         output_filename = "{name} - Time Off Request.docx".format(name = emp_name)
         doc.save(output_filename)
-
-        # Serve the file for download
         return send_file(output_filename, as_attachment=True, download_name=output_filename)
-
+    
     except Exception as e:
         return f"Error generating document: {e}"
 
 if __name__ == '__main__':
+
     app.run(debug=True)
